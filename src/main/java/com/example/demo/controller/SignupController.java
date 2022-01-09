00f5d75eaf1013b33ae0757991f223135a4e6df3
trinhtuan1 <trinhminhtuan.6868@gmail.com>
2022-01-09 20:55:20 +0700

    before chapter 8

--- src/main/java/com/example/demo/controller/SignupController.java
+++ src/main/java/com/example/demo/controller/SignupController.java
@@ -1,24 +1,32 @@
 package com.example.demo.controller;
 
+import com.example.demo.form.GroupOrder;
+import com.example.demo.form.SignupForm;
 import com.example.demo.service.UserApplicationService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Date;
 import java.util.Map;
 
 @RestController
 @RequestMapping("/user")
+@Slf4j
 public class SignupController {
     @Autowired
     private UserApplicationService userApplicationService;
 
     @GetMapping("/signup")
-    public Map<String, Integer> getSignup() {
+    public Map<String, Integer> getSignup(@ModelAttribute SignupForm form) {
         return userApplicationService.getGenderMap();
     }
 
     @PostMapping("/signup")
-    public String postSignup() {
-        return "redirect:/login";
+    public String postSignup(@ModelAttribute @Validated(GroupOrder.class) SignupForm form) {
+        return form.toString();
     }
 }
---
+++ src/main/java/com/example/demo/form/SignupForm.java
@@ -0,0 +1,43 @@
+package com.example.demo.form;
+
+import lombok.Data;
+import org.hibernate.validator.constraints.Length;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.validation.constraints.*;
+import java.util.Date;
+
+@Data
+public class SignupForm {
+    @NotBlank (groups = ValidGroup1.class)
+    @Email (groups = ValidGroup2.class)
+    private String userId;
+
+    @NotBlank(groups = ValidGroup1.class)
+    @Length(min=4, max=100, groups = ValidGroup2.class)
+    @Pattern(regexp="^[a-zA-Z0-9]+$", groups = ValidGroup2.class)
+    private String password;
+
+    @NotBlank(groups = ValidGroup1.class)
+    private String userName;
+
+    @DateTimeFormat(pattern="dd/MM/yyyy")
+    @NotNull(groups = ValidGroup1.class)
+    private Date birthday;
+
+    @Min(value = 20, groups = ValidGroup2.class)
+    @Max(value = 100, groups = ValidGroup2.class)
+    private Integer age;
+
+    @NotNull(groups = ValidGroup1.class)
+    private Integer gender;
+
+    public SignupForm(String userId, String password, String userName, Date birthday, Integer age, Integer gender) {
+        this.userId = userId;
+        this.password = password;
+        this.userName = userName;
+        this.birthday = birthday;
+        this.age = age;
+        this.gender = gender;
+    }
+}
