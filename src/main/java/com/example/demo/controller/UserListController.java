3121bfce1919d56deab5724f306f1efca4b59900
TrinhTuan <trinhminhtuan.6868@gmail.com>
2022-01-22 00:00:05 +0700

    search user list by userId and userName

--- src/main/java/com/example/demo/controller/UserListController.java
+++ src/main/java/com/example/demo/controller/UserListController.java
@@ -2,8 +2,11 @@ package com.example.demo.controller;
 
 import com.example.demo.domain.user.model.MUser;
 import com.example.demo.domain.user.service.UserService;
+import com.example.demo.form.UserListForm;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,9 +18,12 @@ public class UserListController {
     @Autowired
     private UserService userService;
 
+    @Autowired
+    private ModelMapper modelMapper;
+
     @GetMapping("/list")
-    public List<MUser> getUserList() {
-        List<MUser> userList = userService.getUsers();
-        return userList;
+    public List<MUser> getUserList(@ModelAttribute UserListForm form) {
+        MUser user = modelMapper.map(form, MUser.class);
+        return userService.getUsers(user);
     }
 }
--- src/main/java/com/example/demo/domain/user/service/UserService.java
+++ src/main/java/com/example/demo/domain/user/service/UserService.java
@@ -8,7 +8,7 @@ import java.util.List;
 public interface UserService {
     public void signup(MUser user);
 
-    public List<MUser> getUsers();
+    public List<MUser> getUsers(MUser user);
 
     public MUser getOneUser(String userId);
 
