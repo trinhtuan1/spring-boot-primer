78d44f93077ea832ac732fe6f85be560968dd996
TrinhTuan <trinhminhtuan.6868@gmail.com>
2022-01-25 15:12:09 +0700

    handle @Around annotation aspect

--- src/main/java/com/example/demo/aspect/LogAspect.java
+++ src/main/java/com/example/demo/aspect/LogAspect.java
@@ -1,8 +1,11 @@
 package com.example.demo.aspect;
 
 import lombok.extern.slf4j.Slf4j;
+import org.apache.tomcat.jni.Proc;
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.data.relational.core.sql.Join;
@@ -21,4 +24,20 @@ public class LogAspect {
     public void endLog(JoinPoint jp) {
         log.info("End method " + jp.getSignature());
     }
+
+    //    @Around("bean(*Controller)")
+    //    @Around("@annotation(org.springframework.web.bind.annotation.GetMapping)")
+    @Around("@within(org.springframework.web.bind.annotation.RestController)")
+    public Object startLog(ProceedingJoinPoint jp) throws Throwable {
+        log.info("Method start: " + jp.getSignature());
+        try {
+            Object result = jp.proceed();
+            log.info("Method end: " + jp.getSignature());
+            return result;
+        } catch (Exception e) {
+            log.error("Method abend: " + jp.getSignature());
+            throw e;
+        }
+    }
+
 }
