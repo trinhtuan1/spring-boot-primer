44a86c3fb8fd0be7e965dfceadc1e988e28594fc
TrinhTuan <trinhminhtuan.6868@gmail.com>
2022-01-26 09:33:29 +0700

    prohibition of direct link

---
+++ src/main/java/com/example/demo/config/SecurityConfig.java
@@ -0,0 +1,29 @@
+package com.example.demo.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring()
+                .antMatchers("/webjars/**")
+                .antMatchers("/css/**")
+                .antMatchers("/js/**")
+                .antMatchers("/h2-console");
+    }
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/login").permitAll()
+                .antMatchers("/user/signup").permitAll()
+                .anyRequest().authenticated();
+        http.csrf().disable();
+    }
+}
